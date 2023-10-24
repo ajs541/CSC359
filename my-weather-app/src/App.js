85d4845a4aca2e25db65cf07914053046fe6e4a2
ajs541 <ajs541stand@gmail.com>
2023-10-24 14:03:42 -0500

    oh god i decided to mess with JS files

---
+++ my-weather-app/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React, { useEffect, useState } from "react";
+export default function App() {
+
+  const [lat, setLat] = useSate([]); //State for Latitude
+  const [long, setLong] = useState([]); //State for Longitude
+
+  //useEffect loads functions when application loads up and is reloaded.
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+  });
+
+  console.log("Latitude is:", lat)
+  console.log("Longitude is:", long)
+  }, [lat, long]);
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
