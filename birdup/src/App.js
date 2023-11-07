3a6ff6177a1558df4af1a87341bc2a055d0aeb57
ajs541 <ajs541stand@gmail.com>
2023-11-07 10:58:40 -0600

    further changes on Excel

--- birdup/src/App.js
+++ birdup/src/App.js
@@ -2,6 +2,14 @@ import './App.css';
 import Excel from './components/Excel';
 
 function App() {
+
+  const headers = localStorage.getItem( 'headers' );
+  const data = localStorage.getItem( 'data' );
+
+  if (!headers) {
+    headers = ['Title', 'Year', 'Rating', 'Comments'];
+    data = [['Red Whine', '2021', '3', 'meh']];
+  }
   return (
     <div>
       <Excel
@@ -14,3 +22,4 @@ function App() {
     </div>
   );
 }
+export default App;
