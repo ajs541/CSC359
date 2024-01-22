94a92a7238712fcac4f962dbd13b13d14b98e643
ajs541 <ajs541stand@gmail.com>
2024-01-22 16:20:03 -0600

    Added HashMap and SepChainMap

---
+++ Python/HashMap.py
@@ -0,0 +1,37 @@
+from BaseMap import BaseMap
+
+class HashMap(BaseMap):
+    
+    def __init__(self, capacity=None):
+        
+        self._bucket = [None] * self.__capacity
+        if capacity == None:
+            self.__capacity=11
+        else:
+            self.__capacity=capacity
+        self._count=0
+        self.__prime=16937
+        self.__scale=277
+        self.__offset=1033
+    
+    def __len__(self):
+        
+        return self._count
+    
+    def __hashFunction(self, key):
+        
+        h=hash(key)
+        index = (((h*self.__scale)+self.__offset)%self.__prime)%self.__capacity
+        return index
+    
+    def __setitem__(self, key, value):
+        
+        index=self.__hashFunction(key)
+        self._bucket_setitem(key, value, index)
+    
+    def __delitem__(self,key):
+        
+        index = self.__hashFunction(key)
+        self._bucket_delitem(key, index)
+        
+    
---
+++ Python/SepChainMap.py
@@ -0,0 +1,30 @@
+from HashMap import HashMap
+from UnsortedMap import UnsortedMap
+
+class SepChainMap(HashMap):
+    
+    def __init__(self, capacity=None):
+        
+        super().__init__(capacity)
+        
+    def _bucket_getitem(self, key, value):
+        
+        if self._bucketArray[index]==None:
+            raise KeyError()
+        return self._bucketArray[index][key]
+    
+    def _bucket_setitem(self, key, value, index):
+        
+        if self._bucketArray[index]==None:
+            self._bucketArray[index] = UnsortedMap()
+        oldlen = len(self._bucketArray[index])
+        self._bucketArray[index][key] = value
+        newlen = len(self._bucketArray[index])
+        self._count = self._count + (newlen - oldlen)
+    
+    def _bucket_delitem(self, key, index):
+        
+        if self._bucketArray[index]==None:
+            raise KeyError()
+        del self._bucketArray[index][key]
+        self._count=self._count-1
