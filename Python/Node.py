e400b607127523a8b9256c3474734a54a3b9578c
ajs541 <ajs541stand@gmail.com>
2024-01-29 10:56:34 -0600

    Finishing up SLL

--- Python/Node.py
+++
@@ -1,7 +0,0 @@
-
-class Node:
-    
-    def __init__(self, next, data):
-        
-        self.next = next
-        self.data = data
--- Python/SingleLinkedList.py
+++ Python/SingleLinkedList.py
@@ -1,4 +1,11 @@
-from Node import Node
+# Aaron Standefer (with help from Mark Smith)
+
+class Node:
+    
+    def __init__(self, next, data):
+        
+        self.next = next
+        self.data = data
 
 class SingleLinkedList:
     
@@ -12,8 +19,8 @@ class SingleLinkedList:
         if self.__count == 0:
             self.__tail = newNode
         else:
-            newNode.next = head
-        head = newNode
+            newNode.next = self.__head
+        self.__head = newNode
         self.__count += 1
     
     def addLast(self, element):
@@ -48,10 +55,70 @@ class SingleLinkedList:
         if self.__count == 1:
             self.__tail = None
         self.__head = self.__head.next
-        self.__count += 1
+        self.__count -= 1
         return element
     
     def removeLast(self):
         if self.__count == 0:
             raise Exception("Cannot remove element from empty list")
+        element = self.__tail.data
+        if self.__count == 1:
+            self.__head = None
+            self.__tail = None
+        else:
+            newTail = self.__at(self.__count - 2)
+            newTail.next = None
+            self.__tail = newTail
+        self.__count -= 1
+        return element
+    
+    def remove(self, pos):
+        if self.__count == 0:
+            raise Exception("Cannot remove element from empty list")
+       # element = self.__at(pos).data
+        if self.__count == 1:
+            element = self.__head.data
+            self.__head = None
+            self.__tail = None
+            self.__count -= 1
+            return element
+        if pos == 0:
+            return self.removeFirst()
+        if pos == self.__count:
+            return self.removeLast()
+        else:
+            newPos = self.__at(pos-1)
+            element = newPos.next.data
+            newPos.next = newPos.next.next
+            self.__count -= 1
+            return element
+            
+            
+    def get(self, pos):
+        if self.__count == 0:
+            raise Exception("List is empty")
+        return self.__at(pos).data
+    
+    def __contains__(self, data):
         
+        current = self.__head
+        while current != self.__tail:
+            if current.data == data:
+                return True
+            current = current.next
+        return False
+        
+    def __at(self, index):
+        if index == None or index < 0 or index >= self.__count:
+            raise Exception("Index not in bounds of list")
+        current = self.__head
+        for _ in range(index):
+            current = current.next
+        return current
+    
+    def __iter__(self):
+        node = self.__head
+        while node != self.__tail:
+            yield node.data
+            node = node.next
+        yield node.data
