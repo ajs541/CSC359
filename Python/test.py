37900df2f7d79ae39a6bd8647b6bdd46b10c2e4e
ajs541 <ajs541stand@gmail.com>
2024-01-30 14:57:02 -0600

    tester stuff

--- Python/test.py
+++ Python/test.py
@@ -16,10 +16,46 @@ while not finish:
         i = 0
 """
 
-import queue
+def moveZeroesLeft(list):
+    zeroList = []
+    otherList = []
+    for i in range(len(list)):
+        if list[i] == 0:
+            zeroList.append(list[i])
+        else:
+            otherList.append(list[i])
+    return zeroList + otherList
 
-newQ = queue.queue()
+def distinctInts(list):
+    exists = []
+    for i in range(len(list)):
+        if list[i] not in exists:
+            print(list[i])
+            exists.append(list[i])
 
+def leftRightTriangle(int):
+    if int <= 0:
+        raise ValueError("cannot make a zero or negative triangle")
+    count = 0
+    for i in range(int):
+        stars = ""
+        for j in range(count+1):
+            stars += "* "
+        print(stars)
+        count += 1
 
+def rightRightTriangle(int):
+    if int <= 0:
+        raise ValueError("no empty triangles")
+    count = int-1
+    for i in range(int):
+        stars = "  " *count
+        stars += " *" *(int-count)
+        print(stars)
+        count -= 1
+        
 
-    
+if __name__ in "__main__":
+    L = [3, 0, 4, 6, 0, 3]
+    leftRightTriangle(4)
+    rightRightTriangle(4)
