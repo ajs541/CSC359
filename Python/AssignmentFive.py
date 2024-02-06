1defb94dc9cbafc28a727302dc91ba23e0c23aa7
ajs541 <ajs541stand@gmail.com>
2024-02-06 17:03:47 -0600

    Finished Question 1, commenting about code and which functions are for which questions.

--- Python/AssignmentFive.py
+++ Python/AssignmentFive.py
@@ -1,19 +1,33 @@
 # Aaron Standefer (with help from Mark Smith)
-import math
 from UnsortedMap import UnsortedMap
 from Stack import Stack
-#for question one, dictionary is very mutable,
-#and sorted("string") => ['s', 't', 'r', 'i', 'n', 'g']
+#Question 1
+#Ended up making two functions for Question 1 since I needed to do something multiple times, easier to read and follow
 def anagramDetect(lst):
-    letters = UnsortedMap()
-    sort = sorted(lst[0])
-    for i in range(len(sort)):
-        if letters.get(sort[i]) != None:
-            letters[sort[i]] = letters[sort[i]] + 1
+    complete = []
+    for i in range(len(lst)):
+        if complete.__contains__(lst[i]):
+            continue
+        printString = lst[i]
+        word1 = sortToDictionary(lst[i])
+        for j in range(i+1, len(lst)):
+            word2 = sortToDictionary(lst[j])
+            if word1 == word2:
+                printString += "," + lst[j]
+                complete.append(lst[j])
+        complete.append(lst[i])
+        print(printString)
+#Takes string, sorts char's, and then puts into a dictionary.  Returns Dictionary.
+def sortToDictionary(string):
+    sortString = sorted(string)
+    dictionary = UnsortedMap()
+    for i in range(len(sortString)):
+        if dictionary.get(sortString[i]) != None:
+            dictionary[sortString[i]] = dictionary[sortString[i]] + 1
         else:
-            letters[sort[i]] = 1
-    print(letters)
-
+            dictionary[sortString[i]] = 1
+    return dictionary
+#Question 2
 def mergeSortLists(list1, list2):
     newList = []
     count = 0
@@ -49,7 +63,7 @@ def mergeSortLists(list1, list2):
     if newList.__contains__(list2[j]) == False and list2[j] > list1[i]:
         newList.append(list2[j])                
     return newList
-
+#Question 3
 def bracketChecker(string):
     openStacker = Stack()
     for i in range(len(string)):
@@ -86,7 +100,7 @@ def bracketChecker(string):
             return False
     except Exception:
         return True
-
+#Question 4
 def intOccur(lst):
     occur = UnsortedMap()
     for i in range(len(lst)):
@@ -95,7 +109,7 @@ def intOccur(lst):
         else:
             occur[lst[i]] = 1
     print(occur)
-
+#Question 5
 def getPrimes(lst):
     primeList = []
     for i in range(len(lst)):
@@ -116,14 +130,15 @@ def getPrimes(lst):
                     
 
 if __name__ == "__main__":
-    l2 = [7919, 7907, 7901, 7883, 7879, 7877, 7873, 7867, 7853, 7841, 7829, 7823, 7817, 7793, 7789, 7759, 7757, 7753, 7741, 7727, 2**82589933 - 1]
+    stupidLargePrime = 2**82589933 - 1
+    l2 = [7919, 7907, 7901, 7883, 7879, 7877, 7873, 7867, 7853, 7841, 7829, 7823, 7817, 7793, 7789, 7759, 7757, 7753, 7741, 7727]
     l1 = [3, 9, 11, 14, 15, 25]
     l3 = [4, 6, 10]
     print(mergeSortLists(l1,l2))
     getPrimes(l2)
     l4 = [4, 6, 4, 10, 33, 10]
     intOccur(l4)
-    l5 = ['act']
+    l5 = ['act', 'abba', 'why', 'bbaa', 'abab', 'tac', 'hi', 'who', 'how']
     anagramDetect(l5)
     l6 = '{(})<>[]'
     print(bracketChecker(l6))
